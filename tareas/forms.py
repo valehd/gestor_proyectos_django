5664226602e5bb869c0fb9874175e0e5d25d4d8f
valehd <valehd.92@gmail.com>
2026-09-13 09:56:49 -0300

    agrego validaciones en formularios

--- tareas/forms.py
+++ tareas/forms.py
@@ -16,7 +16,40 @@ class ProyectoForm(forms.ModelForm):
         model = Proyecto
         fields = ["nombre", "descripcion"]
 
+    def clean_nombre(self):
+        nombre = self.cleaned_data["nombre"].strip()
+
+        if len(nombre) < 3:
+            raise forms.ValidationError(
+                "El nombre debe tener al menos 3 caracteres."
+            )
+
+        return nombre
+
 class TareaForm(forms.ModelForm):
     class Meta:
         model = Tarea
         fields = ["titulo", "descripcion", "estado"]
+
+    def clean_titulo(self):
+        titulo = self.cleaned_data["titulo"].strip()
+
+        if len(titulo) < 3:
+            raise forms.ValidationError(
+                "El título debe tener al menos 3 caracteres."
+            )
+
+        return titulo
+
+
+    def clean_descripcion(self):
+        descripcion = self.cleaned_data["descripcion"].strip()
+
+        if len(descripcion) < 5:
+            raise forms.ValidationError(
+                "La descripción debe tener al menos 5 caracteres."
+            )
+
+        return descripcion
+
+    
